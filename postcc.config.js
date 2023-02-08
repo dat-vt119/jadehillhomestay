f00e3802d40250b8fa1180563e83d2eee2a56c52
dat-vt <dat.vt204723@sis.hust.edu.vn>
2023-02-09 00:17:50 +0700

    first commit

---
+++ postcc.config.js
@@ -0,0 +1,11 @@
+const tailwindcss = require("tailwindcss");
+
+module.exports = {
+  plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
+};
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route } from "react-router-dom";
+// import Home from "./components/Home";
+// import Login from "./form/Login";
+import Login from "./pages/Auth/Login";
+// import DashBoard from "./pages/DashBoard/DashBoard";
+// import AmenityForm from "./components/HomeStay/CreateHomestay/AmenityForm";
+// import HomePage from "./homepage/Component/HomePage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {/* <Route exact path="/" element={<HomePage />}></Route> */}
+      <Route path="/login" element={<Login />}></Route>
+      {/* <Route path="/dashboard" element={<DashBoard />} /> */}
+      {/* <Route path="/amenity" element={<AmenityForm />} /> */}
+    </Routes>
   );
 }
 
